e48b024be340bde299243c6d6a0f5173fd15f24d
Aleksandar Djindjic <djindjic@gmail.com>
2015-04-17 07:30:06 +0200

    back to # links
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/app/config/main.config.js
+++ app/assets/javascripts/app/config/main.config.js
@@ -1,11 +1,11 @@
 //= require_self
 
-(function() {
-  'use strict';
-  angular.module('id5').
-      config(['$locationProvider',
-        function locationProviderConfig($locationProvider) {
-          $locationProvider.html5Mode(true).hashPrefix('!');
-        },
-      ]);
-})();
+// (function() {
+//   'use strict';
+//   angular.module('id5').
+//       config(['$locationProvider',
+//         function locationProviderConfig($locationProvider) {
+//           $locationProvider.html5Mode(true).hashPrefix('!');
+//         },
+//       ]);
+// })();
