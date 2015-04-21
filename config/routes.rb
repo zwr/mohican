72b796853bdc0525b7627fee4455e3ef96012d03
Aleksandar Djindjic <djindjic@gmail.com>
2015-04-21 14:30:44 +0200

    more drying
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -12,36 +12,4 @@ Rails.application.routes.draw do
 
   get 'index_old', to: 'home#index_old'
   root to: 'home#index'
-
-  #angular routes
-  get '/start', to: 'home#index'
-  get '/map', to: 'home#index'
-  get '/design', to: 'home#index'
-  get '/history', to: 'home#index'
-  get '/customers', to: 'home#index'
-  get '/deliveries', to: 'home#index'
-  get '/activities', to: 'home#index'
-  get '/routes', to: 'home#index'
-  get '/resources', to: 'home#index'
-  get '/timetables', to: 'home#index'
-  get '/links', to: 'home#index'
-  get '/groups', to: 'home#index'
-  get '/hours-reports', to: 'home#index'
-  get '/hours-accounting', to: 'home#index'
-
-  get '/start/:page', to: 'home#index'
-  get '/map/:page', to: 'home#index'
-  get '/design/:page', to: 'home#index'
-  get '/history/:page', to: 'home#index'
-  get '/customers/:page', to: 'home#index'
-  get '/deliveries/:page', to: 'home#index'
-  get '/activities/:page', to: 'home#index'
-  get '/routes/:page', to: 'home#index'
-  get '/resources/:page', to: 'home#index'
-  get '/timetables/:page', to: 'home#index'
-  get '/links/:page', to: 'home#index'
-  get '/groups/:page', to: 'home#index'
-  get '/hours-reports/:page', to: 'home#index'
-  get '/hours-accounting/:page', to: 'home#index'
-  #end angular routes
 end
