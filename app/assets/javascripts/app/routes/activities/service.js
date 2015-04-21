785ba5268895ae8945967605dcbb1cb774f76b0c
zeljko <zeljko@z-ware.fi>
2015-04-21 19:06:09 +0300

    eager loading, fucking fantastic
    This is simply the best commit I have ever made!
    
    Here is what is going on. The service loads firstFetchSize or records,
    which should be set to a number larger than the page size, but of size
    that is fast processed by both the back end and front end loading. In
    existing setup, 200 records are loaded and 20 are returned to show in
    the page.
    
    Even before the page records are returned, another fetchSize count
    of records is requested, records that succees those already got, and when
    those arrive, another set of preceeding record is requested, until all
    records of the current filter are received.
    
    This way records are received in background while they are shown as soon
    as some 'significant minimum' is received.
    
    There is several steps still pending:
    
    * Seriouv testing of marginal cases. It might be some offsetting is
      messed up. Also, need to test what happens when you interrupt loading
      by changing filter, for instance.
    * First load should not really be the page and beyond, but around the
      page, which would help if user immediatelly moves to the previous page.
      It should not be assumed that page 1 is loaded first, on the contrary.
    * Of course, service should be generalized, and become mohican, and any
      reference to 'activity' should be in extended object only.
    * Finally, we should think what happens if you ask for a page that has not
      yet been loaded. It might be so that wizest (sic) is just to wait, as
      loading is still fast.
    * Also important is to limit how many documents would in end be laoded.
      3070 in this test case is not a lot for Chrome on a storng machine, but
      there could be many more. Then we should stop before full filter is loaded.
    
    But nevertheless, this is fucking fantastic commit!
    
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- app/assets/javascripts/app/routes/activities/service.js
+++ app/assets/javascripts/app/routes/activities/service.js
@@ -1,5 +1,3 @@
-//= require_self
-
 (function() {
   'use strict';
 
@@ -7,73 +5,195 @@
       .module('id5.services')
       .factory('activitiesService', ['mnBaseService', '$http', '$q', ActivitiesService]);
 
+  var DEBUG = true;
+  function trace(text) {
+    if(DEBUG) {
+      console.log(text);
+    }
+  }
+
   function ActivitiesService(mnBaseService, $http, $q) {
     var service = {};
-    mnBaseService.extendsTo(service);
+    // We will activate the following later
+    // mnBaseService.extendsTo(service);
 
-    service.layout = null;
     service.buffer = null;
+    // Following is used to calculate the actual index of the required element
     service.pageSize = 20;
+    // Following points out in which direction to grow the buffer. First it will
+    // actually move forward, as this flag is flipped before the http request
+    // is sent.
+    service.nextEagerGrowthForward = false;
+    // Following constant shows the size of fetch. It must by all means be
+    // larger than pageSize
+    service.fetchSize = 100;
+    service.firstFetchSize = 200;
+    // Following is the bottom and top index of the buffer data. At some point,
+    // when it is completelly loaded, these two will be 0 and totalCount.
+    // Indexes are zero based, which means that when the eager loading is
+    // complete, topIndex == totalCount - 1.
+    service.bottomIndex;
+    service.topIndex;
+    service.totalCount;
+    // Following is the ongoing promise. This is the only promise that can
+    // run against the server. If this is already take (not null), we need to
+    // wait for it to fulfill and only .then set our own promise.
+    service.thePromise = null;
+    // Following is set to true when eager loading starts. Setting this to false
+    // interupts eager loading, so that we can start it all over again. See
+    // where it is set to false to understand why we would do it.
+    service.beEager = false;
+    // Following becomes true when all the filter records have been retrieved
+    service.fullyLoaded = false;
 
-    service.getItem = function(id) {
-      if(service.buffer) {
-        for(var i = 0; i < service.buffer.length; i++) {
-          if(service.buffer[i].Order_ID === id) {
-            return $q.when(service.buffer[i]);
-          }
-        }
-        return $q.when(null);
+    // pageIndex is 1 based!
+    service.getPage = function(pageIndex) {
+      // If we have the page, return the page
+      if((pageIndex - 1) * service.pageSize >= service.bottomIndex
+         && pageIndex * service.pageSize - 1 <= service.topIndex) {
+        trace("got page " + pageIndex);
+        return $q.when(service.buffer.slice(
+          (pageIndex - 1) * service.pageSize - service.bottomIndex,
+          pageIndex * service.pageSize - service.bottomIndex
+        ));
       } else {
-        return service._fetchData()
-          .then(function() {
-            return service.getItem(id);
+        return service.fetchEagerly((pageIndex - 1) * service.pageSize)
+        .then(function() {
+          return service.getPage(pageIndex);
+        });
+      }
+    }
+
+    service.fetchEagerly = function(startIndex) {
+      //reset the buffer with the following command
+      service.fullyLoaded = false;
+      // if there is an ongoing promise, wait for it to complete after making
+      // sure there will be no continuation
+      if(service.thePromise != null) {
+        service.beEager = false;
+        return service.thePromise.then(function() {
+          return service.fetchEagerly(startIndex);
+        })
+      } else {
+        // fetch now, and then continue eagerly
+        service.beEager = true;
+        trace("get activities offset = " + startIndex
+          + " count = " + service.firstFetchSize);
+        return service.thePromise = $http.get('/api/activities?offset=' +
+            startIndex + '&count=' + service.firstFetchSize)
+          .then(function(resp) {
+            trace("  - activities received");
+            service.thePromise = null;
+            service.buffer = resp.data.items;
+            service.totalCount = resp.data.total_count;
+            service.bottomIndex = resp.data.offset;
+            service.topIndex = service.bottomIndex + resp.data.items.length;
+            service.continueEagerly();
+            return service.buffer;
           });
       }
-    };
-
-    service.getPage = function(pageNo) {
-      if(service.buffer) {
-        return $q.when(service.buffer.slice((pageNo - 1) * service.pageSize, pageNo * service.pageSize));
-      } else {
-        return service._fetchData()
-          .then(function() {
-            return service.getPage(pageNo);
-          });
-      }
-    };
+    }
 
     service.getPageCount = function() {
       if(service.buffer) {
-        return $q.when(parseInt((service.buffer.length - 1) / service.pageSize + 1));
+        var page_count = parseInt(
+          (service.totalCount - 1) / service.pageSize + 1);
+        trace("got page count " + page_count
+          + " item count = " + service.totalCount);
+        return $q.when(page_count);
+      } else if(service.thePromise) {
+        return service.thePromise.then(function() {
+          // Somebody is already getting something, which will probably
+          // do what we need, so just wait for that promise to fullfil and
+          // then try again the same.
+          return service.getPageCount();
+        });
       } else {
-        return service._fetchData()
+        return service.getPage(1)
           .then(function() {
             return service.getPageCount();
           });
       }
     };
 
+    Array.prototype.append = function(other_array) {
+      other_array.forEach(function(v) {this.push(v)}, this);
+      return this;
+    }
+
+    service.continueEagerly = function() {
+      if(service.beEager) {
+        if(service.bottomIndex == 0) {
+          service.nextEagerGrowthForward = true;
+        } else if(service.topIndex == service.totalCount - 1) {
+          service.nextEagerGrowthForward = false;
+        } else {
+          service.nextEagerGrowthForward = !service.nextEagerGrowthForward;
+        }
+        // now find start and count
+        var count = service.fetchSize;
+        var start;
+        if(service.nextEagerGrowthForward) {
+          start = service.topIndex;
+          if(service.totalCount - service.topIndex < count ) {
+            count = service.totalCount - service.topIndex;
+          }
+        } else {
+          start = service.bottomIndex - service.fetchSize;
+          if(start < 0 ) {
+            // note that start is negative, so this lowers the count
+            count += start;
+            start = 0;
+          }
+        }
+        if(count==0) {
+          service.fullyLoaded = true;
+          return;
+        }
+        trace("get activities offset = " + start
+          + " count = " + count);
+        service.thePromise = $http.get('/api/activities?offset='
+            + start + '&count=' + count)
+          .then(function(resp) {
+            service.thePromise = null;
+            trace("  - activities received");
+            // if we were told to stop, just do nothing
+            if(service.beEager) {
+              if(service.nextEagerGrowthForward) {
+                service.topIndex += resp.data.items.length;
+                service.buffer.append(resp.data.items);
+              } else {
+                service.bottomIndex -= resp.data.items.length;
+                service.buffer = resp.data.items.append(service.buffer);
+              }
+              service.continueEagerly();
+            }
+          });
+      }
+    }
+
+    // layout getting is completelly independent
+    service.layout = null;
+    service.theLayoutPromise = null;
     service.getLayoutDefinitions = function() {
-      if(service.buffer) {
+      if(service.layout) {
         return $q.when(service.layout);
+      } else if(service.theLayoutPromise) {
+        return service.theLayoutPromise.then(function() {
+          service.getLayoutDefinitions();
+        });
       } else {
-        return service._fetchData()
-          .then(function() {
-            return service.getLayoutDefinitions();
+        trace("get activities layout");
+        return service.getLayoutPromise = $http.get('/api/activities/layout')
+          .then(function(resp) {
+            trace("  - activities layout received");
+            service.getLayoutPromise = null;
+            service.layout = resp.data.layout;
+            return service.layout;
           });
       }
     };
 
-    service._fetchData = function() {
-      return $http.get('/api/activities')
-        .then(function(responseActivities) {
-          service.buffer = responseActivities.data.items;
-          return $http.get('/api/activities/layout')
-          .then(function(responseLayout) {
-            service.layout = responseLayout.data.layout;
-          });
-        });
-    };
     return service;
   }
 })();
