cb329aa45655b1ace0e11a50fae82902a4578ac7
zeljko <zeljko@z-ware.fi>
2015-04-22 16:22:41 +0300

    step 4: complete step 3 also for transcluded text
    This is the same thing, but as transcluded test is treated as
    an array of elements (but we know it is just text), we need a
    little bit different (and more fucked up) approach.
    
    But this gives us a chance to learn how the transclusion can be
    modified. Because of that fifth parameter to the link function I
    have decided to use Angular for UI wherever, whenever. Using that
    I can implement my own little local webkit!
    
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- app/assets/javascripts/mohican/oldDirectives/mnMenuItemRoute.js
+++ app/assets/javascripts/mohican/oldDirectives/mnMenuItemRoute.js
@@ -1,13 +1,18 @@
 angular.module('mnOldDirectives')
-  .directive('mnMenuItemRoute', [function() {
+  .directive('mnMenuItemRoute', ['mnTranslations', function(mnTranslations) {
       'use strict';
       return {
         restrict: 'E',
         scope: {
           sref: '@?',
         },
+        link: function(scope, elem, attr, ctrl, transclude) {
+          transclude(function(clone) {
+            scope.mnText = mnTranslations.t(clone.text());
+          });
+        },
         transclude: true,
-        template: '<a ui-sref-active="mn-menu-item-route-active" ui-sref="{{sref}}" ng-transclude></a>',
+        template: '<a ui-sref-active="mn-menu-item-route-active" ui-sref="{{sref}}"><span>{{mnText}}</span></a>',
         controller: ['$scope', function($scope) {
             $scope.sref = $scope.sref || 'base.start';
           },
