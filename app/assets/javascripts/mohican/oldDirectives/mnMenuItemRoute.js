8fdf1f605af86430274809f4c22c250ea57bc407
zeljko <zeljko@z-ware.fi>
2015-04-22 17:06:46 +0300

    even drier: get read of repeated words and the 'base.'
    The sref parameter is already optional, so we can use that
    to populate it ourselves form the transcluded text if not give, for
    case when (not-localized) label is the same as the route.
    
    This qould be supersimple except that ui-sref requires a static
    value and our link function triggers too late. However, we can use
    something called pre-linking function. The link element of a directive
    can either be a function, which is remembered by Angular as
    the postlink, or simply link function, but it can also be an object with
    two elements called pre and post, latter being the link function, but
    the first being the pre-link function, which executes before ANY
    link function.
    
    There are limitations with what pre-link can do, but in our case it
    works just fine. I hope.
    
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- app/assets/javascripts/mohican/oldDirectives/mnMenuItemRoute.js
+++ app/assets/javascripts/mohican/oldDirectives/mnMenuItemRoute.js
@@ -6,17 +6,21 @@ angular.module('mnOldDirectives')
         scope: {
           sref: '@?',
         },
-        link: function(scope, elem, attr, ctrl, transclude) {
-          transclude(function(clone) {
-            scope.mnText = mnTranslations.t(clone.text());
-          });
+        link: {
+          pre: function(scope, elem, attr, ctrl, transclude) {
+            transclude(function(clone) {
+              scope.mnText = mnTranslations.t(clone.text());
+              if(scope.sref == null) {
+                scope.sref = clone.text().toLowerCase()
+              }
+              scope.sref_resolved = 'base.' + scope.sref;
+            });
+          },
+          post: function() {
+          }
         },
         transclude: true,
-        template: '<a ui-sref-active="mn-menu-item-route-active" ui-sref="{{sref}}"><span>{{mnText}}</span></a>',
-        controller: ['$scope', function($scope) {
-            $scope.sref = $scope.sref || 'base.start';
-          },
-        ],
+        template: '<a ui-sref-active="mn-menu-item-route-active" ui-sref="{{sref_resolved}}"><span>{{mnText}}</span></a>',
       };
     },
   ]);
