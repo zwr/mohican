e64f717d3bd656e33fce494eddbda34e32afa223
Aleksandar Djindjic <djindjic@gmail.com>
2015-04-23 10:53:27 +0200

    new routes structure added files
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

---
+++ app/assets/javascripts/app/routes/hoursReports.js
@@ -0,0 +1,30 @@
+//= require_self
+
+(function(mnUtil) {
+  'use strict';
+
+  //arguments: (routeName, controller, service)
+  mnUtil.defineMohicanRoute(
+    'hoursReports',
+    function(resolve) {
+      var ctrl = this;
+      ctrl.get = function() {
+        resolve.pageItems().then(function(result) {
+          ctrl.collection = result;
+        });
+      };
+      ctrl.echo = resolve.echo;
+      ctrl.get();
+    },
+    function(mnBaseService) {
+      var service = {};
+      mnBaseService.extendsTo(service);
+
+      service.echo = function(text) {
+        return text;
+      };
+
+      return service;
+    }
+  );
+})(window.MohicanUtils);
