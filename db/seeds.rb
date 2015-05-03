032d814d729446cb3588c5907585ef1119c509fc
zeljko <zeljko@z-ware.fi>
2015-05-03 20:15:51 +0300

    layout structure more json like
    Layout as taken form Celesta is C# and XML like, and as it is now
    used as JSON, we change the names of some relevant fields.
    
    WARNING! You have to re-seed the database, this commit is not
    compatible with the previous seeds. Simple `rake db:reset` would do.
    
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,36 @@ Layout.delete_all
 puts "Seeding #{activities['items'].length} activities"
 
 activities['items'].each do |a|
+  # We want to store as date those fields that are dates
+  a.as_json.each do |key, value|
+    if value.class == String
+      begin
+        maybe_date = DateTime.parse(value)
+        a[key] = maybe_date
+      rescue
+        # we are fine, it is not a date
+      end
+    end
+  end
   Activity.collection.insert(a)
 end
 
 puts "Seeding activities layouts"
 
-activities_layout  = JSON.parse(IO.read(Rails.root.join('db','seeds','activities_layout.json')))
+activities_layout  = JSON.parse(IO.read(
+  Rails.root.join('db','seeds','activities_layout.json')))
+  new_definition = []
+activities_layout["layouts"][0]["definition"].each do |a|
+  b = {}
+  a.each do |old_key, value|
+    b[:name] = value if old_key == "Name"
+    b[:header] = value if old_key == "HeaderText"
+    b[:width] = value if old_key == "ColumnWidth"
+    b[old_key.to_sym] = value if old_key == old_key.downcase
+  end
+  new_definition << b
+end
+activities_layout["layouts"][0]["definition"] = new_definition
 
 Layout.collection.insert({
     doctype: :activity,
