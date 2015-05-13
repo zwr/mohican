0e14a892474711b03db4d343c05dc03bc189694d
Aleksandar Djindjic <djindjic@gmail.com>
2015-05-13 08:40:24 +0200

    fix: mnQfDateRange watch model
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/directives/mnQfDateRange/controller.js
+++ app/assets/javascripts/mohican/directives/mnQfDateRange/controller.js
@@ -5,19 +5,39 @@
 
   angular
       .module('mohican.directives')
-      .controller('MnQfDateRangeController', ['$timeout', MnQfDateRangeController]);
+      .controller('MnQfDateRangeController', ['$scope', '$timeout', MnQfDateRangeController]);
 
-  function MnQfDateRangeController($timeout) {
+  function MnQfDateRangeController($scope, $timeout) {
     var vm = this;
 
-    if(!vm.model) {
-      vm.model = {startDate: null, endDate: null};
+    if(vm.model) {
+      vm.dateRange = {
+        startDate: vm.model.startDate,
+        endDate: vm.model.endDate,
+      };
+    }
+    else {
+      vm.dateRange = {
+        startDate: null,
+        endDate: null,
+      };
     }
 
-    vm.inputChanged = function() {
-      $timeout(function() {
-        vm.qfChanged({fieldName: vm.field.name});
-      });
-    };
+    $scope.$watch(function() { return vm.dateRange; }, function (newValue, oldValue) {
+      if(newValue !== oldValue) {
+        if(newValue.startDate === null || newValue.endDate === null) {
+          vm.model = undefined;
+        }
+        else {
+          vm.model = {
+            startDate: newValue.startDate,
+            endDate: newValue.endDate,
+          };
+        }
+        $timeout(function() {
+          vm.qfChanged({fieldName: vm.field.name});
+        });
+      }
+    });
   }
 })();
