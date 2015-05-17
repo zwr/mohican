b98dc36fa69517caaf200978a16bb4415cae6960
zeljko <zeljko@z-ware.fi>
2015-05-17 18:08:12 +0300

    optimizing: cache formatted value
    This should ASAP move to the loading, same place where date is
    parsed into date structure, so can the rest be. Create a field
    "formatted" (to Ts) that contains all fields OF ALL LAYOUTS!
    Because you do not know at this point when you would actually load
    which layout.
    
    The benefit of doing it there is that, although it slows down the
    loading of the first page, the rest is presumably invisible for the
    user. As well, there is no longer need for any function being called
    here, but template can just render model.formatted[field.name], which
    saves a stackframe creation.
    
    Signed-off-by: zeljko <zeljko@z-ware.fi>
    
    # Conflicts:
    #	app/assets/javascripts/mohican/directives/mnViewField/controller.js

--- app/assets/javascripts/mohican/directives/mnViewField/controller.js
+++ app/assets/javascripts/mohican/directives/mnViewField/controller.js
@@ -11,18 +11,30 @@
     var vm = this;
 
     vm.formatedModel = function() {
+      if(!angular.isDefined(vm.model.formatted_values)) {
+        vm.model.formatted_values = {};
+      }
+      if(angular.isDefined(vm.model.formatted_values[vm.field.name])) {
+        return vm.model.formatted_values[vm.field.name];
+      }
+
+      var res;
       if(vm.field.view === 'date') {
         vm.field.format = 'DD.MM.YYYY.';//TODO: store format information in db
-        return vm.model ? moment(vm.model).format(vm.field.format) : '';
+        res = (vm.model[vm.field.name] ? moment(vm.model[vm.field.name]).format(vm.field.format) : '');
       }
       else if(_.startsWith(vm.field.view, 'number')) {
         var decimalParams = vm.field.view.slice(7, vm.field.view.length - 1);
-        return angular.isDefined(vm.model) ? vm.model.toFixed(decimalParams) : '';
+        res = (angular.isDefined(vm.model[vm.field.name]) ? vm.model[vm.field.name].toFixed(decimalParams) : '');
       }
       //vm.field.view === 'text'
       else {
-        return vm.model;
+        res = vm.model[vm.field.name];
       }
+      if(!angular.isDefined(res)) {
+        res = null;
+      }
+      return vm.model.formatted_values[vm.field.name] = res;
     };
   }
 })();
