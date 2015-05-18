80d66b18a285ad14376b7ea9b2d5033c593b6a23
zeljko <zeljko@z-ware.fi>
2015-05-18 12:05:25 +0300

    devise-mongoid fix
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- Gemfile
+++ Gemfile
@@ -51,7 +51,7 @@ gem "bson_ext"
 gem "factory_girl_rails", "~> 4.0"
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw]
 gem "tzinfo", platforms: [:mingw, :mswin, :x64_mingw]
-gem "devise", "~> 3.3.0"
+gem "devise"
 
 gem "rest-client"
 gem "pry-rails", :group => :development
--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.action_dispatch.cookies_serializer = :json
+# Following line is deleed so that Mongoid and Devise would work together nice
+# Rails.application.config.action_dispatch.cookies_serializer = :json
