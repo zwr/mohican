a5ec5ec6b19e0296e4b5a888fb2bbc3d40b33560
zeljko <zeljko@z-ware.fi>
2015-05-26 16:25:35 +0300

    inject parameters online wherever possible
    It is way more readable and easier to maintain this way.
    
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- app/assets/javascripts/app/routes/activites.js
+++ app/assets/javascripts/app/routes/activites.js
@@ -4,25 +4,25 @@
   'use strict';
 
   var ROUTE_NAME = 'activities';
-  var Controller = function(resolve, mnGridFilterService, $stateParams, $state, $scope) {
-    _.assign(this, MohicanUtils.mnBaseController);
-    this.initialize(resolve, mnGridFilterService, $stateParams, $state, $scope);
-    this.loadData();
-    this.reportLocation = '/reports';
-    this.attached = false;
-    this.printMe = function(item) {
-      $('#printf').attr('src', "/id.pdf");
-      if(!this.attached) {
-        this.attached = true;
-        $('#printf').load(function() {
-          window.frames["printf"].focus();
-          window.frames["printf"].print();
-        });
-      }
-    };
-  };
-
-  Controller.$inject = [ROUTE_NAME + 'Service', 'mnGridFilterService', '$stateParams', '$state', '$scope'];
+  var Controller = [ROUTE_NAME + 'Service', 'mnGridFilterService', '$stateParams', '$state', '$scope',
+    function(resolve, mnGridFilterService, $stateParams, $state, $scope) {
+      _.assign(this, MohicanUtils.mnBaseController);
+      this.initialize(resolve, mnGridFilterService, $stateParams, $state, $scope);
+      this.loadData();
+      this.reportLocation = '/reports';
+      this.attached = false;
+      this.printMe = function(item) {
+        $('#printf').attr('src', "/id.pdf");
+        if(!this.attached) {
+          this.attached = true;
+          $('#printf').load(function() {
+            window.frames["printf"].focus();
+            window.frames["printf"].print();
+          });
+        }
+      };
+    }
+  ];
 
   //arguments: (routeName, controller, service)
   MohicanUtils.defineMohicanRoute(
