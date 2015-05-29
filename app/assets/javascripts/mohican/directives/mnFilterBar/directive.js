095265aff1748360d3401a9c3a65a68ae5b38ea5
Aleksandar Djindjic <djindjic@gmail.com>
2015-05-29 09:46:45 +0200

    dry up filter bar
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/directives/mnFilterBar/directive.js
+++ app/assets/javascripts/mohican/directives/mnFilterBar/directive.js
@@ -1,27 +1,32 @@
 //= require ./template
 //= require ./controller
 //= require_self
-
-angular.module('mohican.directives')
-  .directive('mnFilterBar', [function() {
-      'use strict';
-      return {
-        restrict: 'E',
-        scope: {
-          layoutChanged: '&',
-          layouts: '=',
-          backendFilterChanged: '&',
-          backendFilters: '=',
-          quickFilterShown: '=',
-          serviceDataLoaded: '=',
-          quickFilterToggled: '&',
-          clearClientSortAndFilter: '&',
-          clientViewLoadingNotification: '=',
-        },
-        templateUrl: 'mohican/directives/mnFilterBar/template.html',
-        controller: 'MnFilterBarController',
-        controllerAs: 'filterBar',
-        bindToController: true,
-      };
-    },
-  ]);
+(function(MohicanUtils) {
+  'use strict';
+  angular.module('mohican.directives')
+    .directive('mnFilterBar', [function() {
+        return {
+          restrict: 'E',
+          scope: {
+            owner: '=?',
+            // layoutChanged: '&',
+            // layouts: '=',
+            // backendFilterChanged: '&',
+            // backendFilters: '=',
+            // quickFilterShown: '=',
+            // serviceDataLoaded: '=',
+            // quickFilterToggled: '&',
+            // clearClientSortAndFilter: '&',
+            // clientViewLoadingNotification: '=',
+          },
+          templateUrl: 'mohican/directives/mnFilterBar/template.html',
+          controller: 'MnFilterBarController',
+          controllerAs: 'filterBar',
+          bindToController: true,
+          link: function(scope, element, attrs, ctrl) {
+            ctrl.owner = scope.owner = MohicanUtils.scopeLookup(scope);
+          },
+        };
+      },
+    ]);
+}(window.MohicanUtils));
