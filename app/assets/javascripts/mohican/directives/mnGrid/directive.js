980b55319073a5ab087500412243733f2f63ba47
Aleksandar Djindjic <djindjic@gmail.com>
2015-05-29 09:12:47 +0200

    extract scoleLookup to MohucanUtils
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/directives/mnGrid/directive.js
+++ app/assets/javascripts/mohican/directives/mnGrid/directive.js
@@ -2,47 +2,42 @@
 //= require ./controller
 //= require_self
 
-angular.module('mohican.directives')
-  .directive('mnGrid', [function() {
+(function(MohicanUtils) {
+  'use strict';
+
+  angular.module('mohican.directives')
+    .directive('mnGrid', [function() {
+        return {
+          restrict: 'E',
+          transclude: true,
+          scope: {
+            owner: '=?'
+          },
+          templateUrl: 'mohican/directives/mnGrid/template.html',
+          controller: 'MnGridController',
+          controllerAs: 'grid',
+          bindToController: true,
+          link: function(scope, element, attrs, ctrl, $transcludeFn) {
+            ctrl.owner = scope.owner = MohicanUtils.scopeLookup(scope);
+            scope.compileItForMe = function(itemScope, itemElement) {
+              $transcludeFn(itemScope, function(notLinkedClone) {
+                itemElement.append(notLinkedClone);
+              });
+            };
+          },
+        };
+      },
+    ]);
+
+  angular.module('mohican.directives')
+    .directive('mnGridActions', [function() {
       'use strict';
       return {
-        restrict: 'E',
-        transclude: true,
-        scope: {
-          owner: '=?'
-        },
-        templateUrl: 'mohican/directives/mnGrid/template.html',
-        controller: 'MnGridController',
-        controllerAs: 'grid',
-        bindToController: true,
-        link: function(scope, element, attrs, ctrl, $transcludeFn) {
-          var i, runner = scope;
-          for(i = 0; (!scope.owner) && i < 10; i++) {
-            if(runner.$parent && runner.$parent.ctrl) {
-              scope.owner = runner.$parent.ctrl;
-              break;
-            }
-            runner = runner.$parent;
-          }
-          ctrl.owner = scope.owner;
-          scope.compileItForMe = function(itemScope, itemElement) {
-            $transcludeFn(itemScope, function(notLinkedClone) {
-              itemElement.append(notLinkedClone);
-            });
-          };
+        scope: false,
+        link: function(scope, element) {
+          scope.ctrl = scope.$parent.owner;
+          scope.$parent.compileItForMe(scope, element);
         },
       };
-    },
-  ]);
-
-angular.module('mohican.directives')
-  .directive('mnGridActions', [function() {
-    'use strict';
-    return {
-      scope: false,
-      link: function(scope, element) {
-        scope.ctrl = scope.$parent.owner;
-        scope.$parent.compileItForMe(scope, element);
-      },
-    };
-  }]);
+    }]);
+}(window.MohicanUtils));
---
+++ app/assets/javascripts/mohican/utils/directives.js
@@ -0,0 +1,14 @@
+(function(MohicanUtils) {
+  'use strict';
+  MohicanUtils.scopeLookup = function(scope) {
+    var i, owner, runner = scope;
+    for(i = 0; (!scope.owner) && i < 10; i++) {
+      if(runner.$parent && runner.$parent.ctrl) {
+        owner = runner.$parent.ctrl;
+        break;
+      }
+      runner = runner.$parent;
+    }
+    return owner;
+  };
+}(window.MohicanUtils));
