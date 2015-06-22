84911c0c738a164af8fc7db3e1f0adaab7748d49
zeljko <zeljko@z-ware.fi>
2015-06-22 21:44:11 +0300

    mn-dialog css: manage position based on height
    This commit was way more difficult than previous.
    
    What happens is again that we only know the width once we have rendered
    everything, thus the timeout - as elements are rendered one by one,
    height checking triggers way too often, triggering Angular's 'stack
    overflow' limit.
    
    As for calculation, we are using the same watch (it is not really possible
    without a watch, but it is not very expensive one) that we used for menu.
    This time we are trying to position the dialog into the middle of the
    window, this we need to also be watching window size.
    
    If dialog is not responsive (and needs to be carefully responsive to the
    window object) it will just glue to left or top and cannot be scrolled.
    So that basically means it has to be responsive.
    
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- app/assets/javascripts/mohican/directives/mnDialogSorcerer/directive.js
+++ app/assets/javascripts/mohican/directives/mnDialogSorcerer/directive.js
@@ -3,11 +3,49 @@
 (function(mohican) {
   'use strict';
   angular.module('mohican.directives')
-    .directive('mnDialogSorcerer', [function() {
+    .directive('mnDialogSorcerer', ['$timeout', function($timeout) {
         return {
           restrict:     'E',
           templateUrl:  'mohican/directives/mnDialogSorcerer/template.html',
           link: function(scope, element, attrs, ctrl) {
+            //  top:200px;left:200px;
+            //scope.mnDialogPosition
+            scope.mnDialogPosition = {
+              left: 200,
+              top: 200
+            };
+            var dialogElem;
+            scope.doCountHeight = true;
+            scope.$watch(
+              function () {
+                dialogElem = element[0].querySelector('.mn-dialog-frame');
+                if(dialogElem && scope.doCountHeight) {
+                  return {
+                    height: dialogElem.clientHeight,
+                    width: dialogElem.clientWidth,
+                    windowHeight: window.innerWidth,
+                    windowWidth: window.innerHeight,
+                  }
+                }
+              },
+              function () {
+                if(dialogElem) {
+                  scope.doCountHeight = true;
+                  $timeout(function() {
+                    scope.mnDialogPosition.left = (window.innerWidth - dialogElem.clientWidth) / 2;
+                    scope.mnDialogPosition.top = (window.innerHeight - dialogElem.clientHeight) / 2;
+                    if(scope.mnDialogPosition.left < 0) {
+                      scope.mnDialogPosition.left = 0;
+                    }
+                    if(scope.mnDialogPosition.top < 0) {
+                      scope.mnDialogPosition.top = 0;
+                    }
+                    scope.doCountHeight = true;
+                  }, 10);
+                }
+              },
+              true
+            );
           }
         };
       }
