fedf7e2772dd3cc5559553fb0e2c33901708d071
Aleksandar Djindjic <djindjic@gmail.com>
2015-07-06 21:40:11 +0200

    cleanup basic-driver again
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/globals/basic-driver.js
+++ app/assets/javascripts/mohican/globals/basic-driver.js
@@ -19,7 +19,6 @@
     });
 
     return function(newCurrentItem) {
-      console.log(newCurrentItem);
       ctrl.items = [];
       if(newCurrentItem[ctrl.collectionName]) {
         ctrl.items = newCurrentItem[ctrl.collectionName];
