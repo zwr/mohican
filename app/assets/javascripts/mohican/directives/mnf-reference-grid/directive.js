8b7191ff02dca5d72421ee8863fefa677fd1b795
Aleksandar Djindjic <djindjic@gmail.com>
2015-08-11 09:08:37 +0200

    popDialog then promise in mnf-reference and mnf-reference-grid directives
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/directives/mnf-reference-grid/directive.js
+++ app/assets/javascripts/mohican/directives/mnf-reference-grid/directive.js
@@ -21,25 +21,25 @@
 
             scope.openRefDialog = function() {
               scope.owner.refResourceController = {};
-              scope.owner.refResourceController.onItemSelect = function(selectedItems) {
-                if(selectedItems.length > 0) {
-                  var selectedItem = selectedItems[0];
-                  scope.mnfDoc._edit[scope.mnfRefId] = selectedItem[scope.owner.refResourceController.primaryKeyName];
-                  scope.mnfDoc['_' + scope.mnfRefId + '_changed'] = true;
-                  var fieldLabel = '';
-                  scope.mnfFieldSource.forEach(function(field) {
-                    fieldLabel += ' ' + selectedItem[field];
-                  });
-                  scope.mnfDoc._edit[scope.mnfField] = _.trim(fieldLabel);
-                  scope.mnfDoc.change(scope.mnfField);
-                  scope.owner.closeDialog();
-                }
-              };
               var service = $injector.get(attr.mnfRefResource + 'Service');
               mohican.extendResourceDriver(scope.owner.refResourceController, service);
               scope.owner.popDialog('Select from ' + attr.mnfRefResource,
                                     'mohican/directives/mnf-reference-grid/ref-resource-dialog.html',
-                                    { hideFooter: true });
+                                    { hideFooter: true }).
+                          then(function(selectedItems) {
+                            if(selectedItems.length > 0) {
+                              var selectedItem = selectedItems[0];
+                              scope.mnfDoc._edit[scope.mnfRefId] = selectedItem[scope.owner.refResourceController.primaryKeyName];
+                              scope.mnfDoc['_' + scope.mnfRefId + '_changed'] = true;
+                              var fieldLabel = '';
+                              scope.mnfFieldSource.forEach(function(field) {
+                                fieldLabel += ' ' + selectedItem[field];
+                              });
+                              scope.mnfDoc._edit[scope.mnfField] = _.trim(fieldLabel);
+                              scope.mnfDoc.change(scope.mnfField);
+                              scope.owner.closeDialog();
+                            }
+                          });
             };
           }
         };
