b889d8911c951f8adaaf752f715d1ca048cc6e3a
Aleksandar Djindjic <djindjic@gmail.com>
2015-08-13 11:24:06 +0200

    mnf-date
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

---
+++ app/assets/javascripts/mohican/directives/mnf-date/directive.js
@@ -0,0 +1,35 @@
+//= require ./template
+//= require_self
+
+angular.module('mohican.directives')
+  .directive('mnfDate', [function() {
+      'use strict';
+      return {
+        scope: {
+          mnfField: '@',
+          mnfLabel: '@'
+        },
+        restrict:    'E',
+        require:     '^mnfForm',
+        templateUrl: 'mohican/directives/mnf-date/template.html',
+
+        link: function(scope, elem, attr, mnfFormCtrl) {
+          scope.mnfDoc = mnfFormCtrl.mnfDoc;
+          scope.$watch(function() {
+            return scope.mnfDoc._state;
+          }, function(newValue) {
+            if(newValue === 'editing') {
+              scope.mnfDoc._edit[scope.mnfField] = new Date(scope.mnfDoc._edit[scope.mnfField]);
+            }
+          });
+          scope.textChanged = function() {
+            scope.mnfDoc['_' + scope.mnfField + '_changed'] = true;
+            scope.mnfDoc._state = 'changed';
+          };
+          scope.dateFormated = function() {
+            return moment(scope.mnfDoc[scope.mnfField]).format('MM/DD/YYYY.');
+          };
+        }
+      };
+    }
+  ]);
