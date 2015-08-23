9d9c25d5a73a0ed115353ba30695cb1ad821ddcf
Aleksandar Djindjic <djindjic@gmail.com>
2015-08-23 20:40:37 +0200

    no delete confirmation for subdoc's grids
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/directives/mnf-delete-grid/directive.js
+++ app/assets/javascripts/mohican/directives/mnf-delete-grid/directive.js
@@ -18,8 +18,11 @@ angular.module('mohican')
           scope.mnOnDelete = angular.isDefined(scope.mnOnDelete) ? scope.mnOnDelete : function() {};
 
           scope.confirmDelete = function() {
-            if((scope.mnfFormGridCtrl.mnfSubdocumetsGrid === true && scope.mnfDoc._state === 'editing') ||
-                scope.mnfFormGridCtrl.mnfSubdocumetsGrid === false && scope.mnfDoc._state === 'ready') {
+            if(scope.mnfFormGridCtrl.mnfSubdocumetsGrid === true && scope.mnfDoc._state === 'editing') {
+              scope.mnfDoc.delete().then(scope.mnOnDelete);
+              mnfFormGridCtrl.currentMnfDoc = null;
+            }
+            if(scope.mnfFormGridCtrl.mnfSubdocumetsGrid === false && scope.mnfDoc._state === 'ready') {
               if($window.confirm('Are you sure that you want to permanently delete document?')) {
                 scope.mnfDoc.delete().then(scope.mnOnDelete);
                 mnfFormGridCtrl.currentMnfDoc = null;
