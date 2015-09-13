932f6bc69385e3a4d635299d447d68bbbf0fbe9e
zeljko <zeljko@z-ware.fi>
2015-09-13 20:30:00 +0300

    dry controller - orders only
    This dries up the OrdersController, pushing everything into the
    Mohican Controller concern. This should be rather reusable accross
    the simple Mohican documents.
    
    Layout is extracted to a YAML file in the app/layouts folder.
    
    Signed-off-by: zeljko <zeljko@z-ware.fi>

--- app/helpers/mohican/controller.rb
+++ app/helpers/mohican/controller.rb
@@ -8,8 +8,16 @@ module Mohican::Controller
     before_action :set_document, only: [:show, :edit, :update, :destroy]
   end
 
-  def the_model
-    self.class.name.remove('Controller').singularize.constantize
+  def resource_name
+    self.class.name.remove('Controller')
+  end
+
+  def doc_name
+    resource_name.singularize
+  end
+
+  def model
+    doc_name.constantize
   end
 
   def index_documents
@@ -26,18 +34,18 @@ module Mohican::Controller
 
     offset = [0, params[:index].to_i - limit / 2].max if params[:index] && offset == 0
 
-    total_count = the_model.all.count
+    total_count = model.all.count
 
     # Make sure you do return something
     limit = [limit, total_count].min
     offset = [offset, total_count - limit].min
 
-    instance_variable_set "@#{self.class.name.remove('Controller').downcase}",
-                          the_model.limit(limit).skip(offset).order_by(id: :asc)
+    instance_variable_set "@#{resource_name.downcase}",
+                          model.limit(limit).skip(offset).order_by(id: :asc)
 
     index_documents
 
-    @documents = instance_variable_get "@#{self.class.name.remove('Controller').downcase}"
+    @documents = instance_variable_get "@#{resource_name.downcase}"
 
     respond_to do |format|
       format.json { render json: { # rubocop:disable all
@@ -49,16 +57,73 @@ module Mohican::Controller
     end
   end
 
-  def show
-    @document = instance_variable_get "@#{self.class.name.remove('Controller').downcase.singularize}"
+  def layout
     respond_to do |format|
-      format.json { render json: @order }
-      format.html { respond_with(@order) }
+      format.json do
+        render json: YAML.load(File.read(Rails.root.join('app', 'layouts', "#{resource_name.downcase}.yml")))
+      end
     end
   end
 
+  def show
+    @document = instance_variable_get "@#{doc_name.downcase}"
+    respond_to do |format|
+      format.json { render json: @document }
+      format.html { respond_with @document }
+    end
+  end
+
+  def new
+    @document = model.new
+    instance_variable_set "@#{doc_name.downcase}", @document
+    respond_with(@document)
+  end
+
+  def edit
+  end
+
+  def create
+    @document = model.new(document_params)
+    @document.creator = current_user
+    respond_to do |format|
+      if @document.save
+        format.html { redirect_to @document, notice: "#{doc_name} was successfully created." }
+        format.json { render json: @document.as_json }
+      else
+        format.html { render :new }
+        format.json { render json: @document.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @document.update(document_params)
+        format.html { redirect_to @document, notice: "#{doc_name} was successfully updated." }
+        format.json { render json: @document.as_json }
+      else
+        format.html { render :new }
+        format.json { render json: @document.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @document.destroy
+        format.json { render json: @document.as_json }
+        format.html { respond_with(@document) }
+      else
+        format.json { render json: @document.errors, status: :unprocessable_entity }
+        format.html { respond_with(@document) }
+      end
+    end
+  end
+
+  # private
+
   def set_document
-    @document = Order.find(params[:id])
-    instance_variable_set "@#{self.class.name.remove('Controller').downcase.singularize}", @document
+    @document = model.find(params[:id])
+    instance_variable_set "@#{doc_name.downcase}", @document
   end
 end
