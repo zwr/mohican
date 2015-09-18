875a51fc085d9b1f60ddc0172d2f316b337e6a7a
Aleksandar Djindjic <djindjic@gmail.com>
2015-09-18 08:46:17 +0200

    mns-button transclusion :ok_hand:
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/directives/mns-button/directive.js
+++ app/assets/javascripts/mohican/directives/mns-button/directive.js
@@ -2,30 +2,27 @@
 //= require_self
 
 angular.module('mohican')
-  .directive('mnsButton', ['$sce', '$compile', '$templateCache', 'mnNotify', function($sce, $compile, $templateCache, mnNotify) {
+  .directive('mnsButton', ['$compile', function($compile) {
       'use strict';
       return {
         restrict: 'A',
 
-        scope: true,
-
+        scope:      true,
         transclude: true,
 
         templateUrl: 'mohican/directives/mns-button/template.html',
 
         compile: function compile(element, attrs) {
-          // element.attr('popover-placement', 'bottom');
-          // element.attr('popover-title', 'Notifications');
-          // var tmpl = $templateCache.get('mohican/directives/mns-button/template.html');
-          // console.log($sce.trustAsHtml(tmpl));
-          // var trustHtml = $sce.trustAsHtml(tmpl);
-          // element.attr('popover-template', 'mnPopoverTpl.html');
+          element.attr('popover-placement', 'bottom');
+          element.attr('popover-title', 'Notifications');
+
+          element.attr('popover-template', '"mnPopoverTpl.html"');
           element.removeAttr('mns-button');
           return {
-            pre: function preLink(scope, iElement, iAttrs, controller) {
-              scope.notifications = [1, 2, 3];
-            },
-            post: function postLink(scope, iElement, iAttrs, controller) {
+            post: function postLink(scope, iElement, iAttrs, controller, transclusionFn) {
+              transclusionFn(function(clone) {
+                iElement.append(clone);
+              });
               $compile(iElement)(scope);
             }
           };
