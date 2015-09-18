a7f74c8c7226e298f756aac3602e932e54765b27
Aleksandar Djindjic <djindjic@gmail.com>
2015-09-18 08:51:26 +0200

    formating source code
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/directives/mns-button/directive.js
+++ app/assets/javascripts/mohican/directives/mns-button/directive.js
@@ -5,25 +5,23 @@ angular.module('mohican')
   .directive('mnsButton', ['$compile', function($compile) {
       'use strict';
       return {
-        restrict: 'A',
-
-        scope:      true,
-        transclude: true,
-
+        restrict:    'A',
+        transclude:  true,
         templateUrl: 'mohican/directives/mns-button/template.html',
 
         compile: function compile(element, attrs) {
           element.attr('popover-placement', 'bottom');
           element.attr('popover-title', 'Notifications');
-
           element.attr('popover-template', '"mnPopoverTpl.html"');
+
           element.removeAttr('mns-button');
+
           return {
-            post: function postLink(scope, iElement, iAttrs, controller, transclusionFn) {
+            post: function postLink(scope, elem, attrs, controller, transclusionFn) {
               transclusionFn(function(clone) {
-                iElement.append(clone);
+                elem.append(clone);
               });
-              $compile(iElement)(scope);
+              $compile(elem)(scope);
             }
           };
         }
