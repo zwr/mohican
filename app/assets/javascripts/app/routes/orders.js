05cf70dc215c3208d2092f47237ff17909c7fc1d
Aleksandar Djindjic <djindjic@gmail.com>
2015-09-25 09:53:05 +0200

    better notification showcase
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/app/routes/orders.js
+++ app/assets/javascripts/app/routes/orders.js
@@ -171,19 +171,8 @@ angular.module('id5').config(['mnRouterProvider', function(mnRouterProvider) {
         ctrl.back = function() {
           $window.history.back();
         };
-        ctrl.addNotif = function(type, fullyClickable, actions, delay) {
-          ctrl.mnNotify.create({
-            message: 'new ' + type + ' notification ' + (ctrl.mnNotify.get().length + 1),
-            type:    type,
-            details: 'long description abot ' + type + ' notification',
-            actions: actions,
-            delay:   delay,
-
-            dismissable:    !fullyClickable,
-            fullyClickable: fullyClickable
-          }).then(function(result) {
-            console.log(result);
-          });
+        ctrl.logFromTemplate = function() {
+          console.log(Array.prototype.slice.call(arguments).join(', '));
         };
         ctrl.addModalNotif = function() {
           ctrl.mnNotify.message('Do you want to save this change before continuing?', 'danger', ['yes', 'no', 'cancel'])
