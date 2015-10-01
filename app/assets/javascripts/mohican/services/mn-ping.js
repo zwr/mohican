1dcf4bb2eda630d64a04413f080292def146fa94
Aleksandar Djindjic <djindjic@gmail.com>
2015-10-01 08:32:12 +0200

    show self dismissable warining for connectivity issues
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/services/mn-ping.js
+++ app/assets/javascripts/mohican/services/mn-ping.js
@@ -7,23 +7,41 @@
 
   function mnPing($http, $interval, mnNotify) {
     var service = {};
+    service.offlineWarning = undefined;
     service.start = function() {
       $interval(function() {
         $http.get('/ping.json').
               then(function (response) {
                 if(response.status === 200) {
+                  if(service.offlineWarning) {
+                    service.offlineWarning.dismiss();
+                    service.offlineWarning = undefined;
+                  }
                   mnNotify.clear();
                 }
                 else {
-                  mnNotify.report(response.status).then(function() {
-                    console.log('ping resolve success');
-                  });
+                  console.log('no 200 response');
+                  // mnNotify.report(response.status).then(function() {
+                  //   console.log('ping resolve success');
+                  // });
                 }
               }).
               catch(function(error) {
-                mnNotify.report(error.status).then(function() {
-                  console.log('ping resolve error');
-                });
+                if(error.status === 401) {
+                  mnNotify.report(error.status).then(function() {
+                    console.log('ping resolve error');
+                  });
+                }
+                else {
+                  if(!service.offlineWarning) {
+                    var warn = mnNotify.warning({
+                      message:     'connectivity problem',
+                      dismissable: false
+                    });
+                    service.offlineWarning = warn.message;
+                  }
+                }
+
               });
       }, 1000);
     };
