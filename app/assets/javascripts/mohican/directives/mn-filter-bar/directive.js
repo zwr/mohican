bfab991fb01fe4e8c2e02cf2ff2353a2bd19f793
Aleksandar Djindjic <djindjic@gmail.com>
2015-10-26 11:28:23 +0100

    mn-filter-bar transluding content
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- app/assets/javascripts/mohican/directives/mn-filter-bar/directive.js
+++ app/assets/javascripts/mohican/directives/mn-filter-bar/directive.js
@@ -13,11 +13,17 @@
           templateUrl:  'mohican/directives/mn-filter-bar/template.html',
           controller:   'MnFilterBarController',
           controllerAs: 'filterBar',
+          transclude:   true,
 
           bindToController: true,
 
-          link: function(scope, element, attrs, ctrl) {
+          link: function(scope, element, attrs, ctrl, transclusionFn) {
             ctrl.owner = scope.owner = mohican.scopeLookup(scope);
+            transclusionFn(scope, function(clone) {
+              console.log(angular.element(clone[1])[0]);
+              angular.element(clone[1]).attr('seleted-document-filters', 'filterBar.seletedDocumentFilters');
+              angular.element(element.find('.mn-translude')[0]).append(clone);
+            });
           }
         };
       }
