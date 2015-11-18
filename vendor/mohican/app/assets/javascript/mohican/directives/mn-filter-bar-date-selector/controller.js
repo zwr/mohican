96b737b0be747c5f648e3ccdf5d9361ef6ba0dd5
Aleksandar Djindjic <djindjic@gmail.com>
2015-11-18 08:49:55 +0100

    fix mn-filter-bar-date-selector
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- vendor/mohican/app/assets/javascript/mohican/directives/mn-filter-bar-date-selector/controller.js
+++ vendor/mohican/app/assets/javascript/mohican/directives/mn-filter-bar-date-selector/controller.js
@@ -12,20 +12,26 @@
 
     vm.setDateFrom = function(days) {
       var today = new Date();
-      today.setHours(0, 0, 0, 0);
-      vm.dateFrom = today.setDate(today.getDate() + days);
+      today.setHours(12, 0, 0, 0);
+      vm.dateFrom = new Date(today.setDate(today.getDate() + days));
     };
 
     vm.getDateFrom = function(days) {
       var today = new Date();
-      today.setHours(0, 0, 0, 0);
-      return today.setDate(today.getDate() + days);
+      today.setHours(12, 0, 0, 0);
+      return new Date(today.setDate(today.getDate() + days));
     };
 
     $scope.$watch(function() {
       return vm.owner.stateMachine.openfilters;
     }, function(newValue, oldValue) {
-      vm.dateFrom = newValue[vm.field] ? new Date(newValue[vm.field]) : undefined;
+
+      if(newValue[vm.field]) {
+        vm.dateFrom = new Date(newValue[vm.field].setHours(12, 0, 0, 0));
+      }
+      else {
+        vm.dateFrom = undefined;
+      }
     });
   }
 })();
